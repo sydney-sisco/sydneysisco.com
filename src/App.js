18443024aeb584034de762eddbffe9e7e145d180
Sydney Sisco <sydney@sydneys-air.lan>
2022-04-02 10:58:08 -0700

    a bit of cleanup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import './App.css';
-
 import Masonry, {ResponsiveMasonry } from 'react-responsive-masonry';
 
+// dynamic import of all images from ./images folder
 function importAll(r) {
   return r.keys().map(r);
 }
-
 const images = importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
 
 const columnsCountBreakPoints = { 350: 1, 750: 2, 900: 3 };
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
       <Masonry gutter={4}>
         {images.map((image) => (
