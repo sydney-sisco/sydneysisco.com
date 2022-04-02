2c234567975e6317cbc9e8fc4562bdbf0f292550
Sydney Sisco <sydney@sydneys-air.lan>
2022-04-02 10:56:36 -0700

    add images with dynamic import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Masonry, {ResponsiveMasonry } from 'react-responsive-masonry';
 
-const images = [
-  "https://cdn.pixabay.com/photo/2020/11/01/19/41/autumn-5704791_1280.jpg",
-  "https://cdn.pixabay.com/photo/2020/07/27/02/30/hands-5441201_1280.jpg",
-  "https://cdn.pixabay.com/photo/2020/11/08/09/41/deer-5723225_1280.jpg",
-  "https://cdn.pixabay.com/photo/2020/11/15/22/08/person-5747420_960_720.jpg",
-  "https://cdn.pixabay.com/photo/2020/09/27/04/15/cat-5605615_960_720.jpg",
-  "https://cdn.pixabay.com/photo/2020/03/14/21/56/wine-4931923_1280.jpg",
-  "https://cdn.pixabay.com/photo/2020/04/19/12/26/thread-5063401_1280.jpg",
-  "https://cdn.pixabay.com/photo/2020/07/11/11/34/mam-tor-5393685_1280.jpg",
+function importAll(r) {
+  return r.keys().map(r);
+}
 
-  "https://cdn.pixabay.com/photo/2020/07/03/14/43/waterdrops-5366584_1280.jpg"
-];
+const images = importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
 
 const columnsCountBreakPoints = { 350: 1, 750: 2, 900: 3 };
-
-
 
 function App() {
   return (
