abebd49955d1a8bded3edc4351f45f7c0460f7bc
Sydney Sisco <sydney@sydneys-air.lan>
2022-04-04 08:52:51 -0700

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+
+function Header() {
+
+  return (
+    <header className='header'>
+      <h1>
+        <a href="/" className='logo'>
+          Sydney Sisco
+        </a>
+      </h1>
+      <h2 className='subheading'>art by human</h2>
+    </header>
+  );
+}
+
+export default Header;
