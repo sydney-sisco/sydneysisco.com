7eaa862d28c3258d8eb74b9d746faf8819079376
Sydney Sisco <sydney@sydneys-air.lan>
2022-04-09 14:07:07 -0700

    adds modal window to gallery images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,7 @@
+import * as React from 'react';
 import './Gallery.css';
-
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Modal from './Modal';
 
 import imageData from '../image-data.json';
 imageData = imageData.reverse();
@@ -11,23 +12,39 @@ const columnsCountBreakPoints = { 350: 1, 815: 2 };
 function importAll(r) {
   return r.keys().map(r).reverse();
 }
-const images = importAll(require.context('../images/gallery/thumbnails/', false, /\.(png|jpe?g|svg)$/));
+const images_tn = importAll(require.context('../images/gallery/thumbnails/', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../images/gallery/', false, /\.(png|jpe?g|svg)$/));
 
 function Gallery() {
+  const [open, setOpen] = React.useState(false);
+  const [modalImage, setModalImage] = React.useState(0);
+  const handleOpen = (i) => {
+    // e.preventDefault();
+    setModalImage(i)
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   return (
+    <>
     <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
       <Masonry gutter={64}>
         {imageData.map(({src, title, data}, i) => (
-          <div>
-            {/* <img src={'/images/' + src} className='image'/> */}
-            <img src={images[i]} className='image' alt={imageData[i]['title']}/>
-            <p className='title'>{imageData[i]['title']}</p>
-            <p className='details'>{imageData[i]['details']}</p>
-          </div>
+          <>
+          <a onClick={()=>handleOpen(i)}>
+            <div className='gallery-item'>
+              {/* <img src={'/images/' + src} className='image'/> */}
+              <img src={images_tn[i]} className='image' alt={imageData[i]['title']}/>
+              <p className='title'>{imageData[i]['title']}</p>
+              <p className='details'>{imageData[i]['details']}</p>
+            </div>
+          </a>
+          </>
         ))}
       </Masonry>
     </ResponsiveMasonry>
+      <Modal open={open} handleClose={handleClose} image={images[modalImage]} alt={imageData[modalImage]['title']}/>
+    </>
   )
 }
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import './Modal.css';
+
+export default function BasicModal({ open, handleClose, image, alt }) {
+
+  console.log(image);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className='box'>
+          <img src={image} className="modal-image" alt={alt} />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
