b517f0b676b968be0d56407e12abdc225cbb2eca
Sydney Sisco <sydney@sydneys-air.lan>
2022-05-06 20:21:48 -0700

    in footer, use a copy event listener to replace obfuscated email address with actual email address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,31 @@
+import React, { useEffect, useRef } from "react";
 import './Footer.css';
 
 function Footer() {
 
+  // use a copy event listener to replace obfuscated email address with actual email address
+  const mailRef = useRef(null);
+
+  useEffect(() => {
+    const span = mailRef.current;
+    // subscribe event
+    span.addEventListener('copy', handleCopy);
+    return () => {
+      // unsubscribe event
+      span.removeEventListener('copy', handleCopy);
+    };
+  }, []);
+
+  const handleCopy = (e) => {
+    e.preventDefault();
+
+    const selection = document.getSelection();
+    e.clipboardData.setData('text/plain', selection.toString().replace('[at]', '@'));
+  }
+
   return (
     <div className='footer'>
-      ©Sydney Sisco  | <span className='animated-border-bottom'>hello[at]sydneysisco.com</span>  | Vancouver, Canada
+      ©Sydney Sisco  | <span ref={mailRef} className='animated-border-bottom'>hello[at]sydneysisco.com</span>  | Vancouver, Canada
     </div>
   );
 }
