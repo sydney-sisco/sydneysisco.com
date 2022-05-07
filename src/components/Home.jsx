7c304e12ea7ee8330bb1815e37960385b1335315
Sydney Sisco <sydney@sydneys-air.lan>
2022-05-06 19:37:56 -0700

    use material ui theme to change button colours

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,22 @@
 import './Home.css';
 import Button from '@mui/material/Button';
 import Slides from './Slides';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#a4b7b5',
+      dark: '#b6c5c4',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#5c6060',
+      dark: '#7c8080',
+      contrastText: '#fff',
+    },
+  },
+});
 
 export default function Home() {
   return (
@@ -11,31 +27,35 @@ export default function Home() {
         <p>Creation unlocks long-forgotten joy. Make your mark before the machines take over.</p>
       </div>
       <div className="gallery-button">
-        <Button
-          href="/gallery"
-          variant="contained"
-          sx={{
-            borderRadius: '300px',
-            backgroundColor: '#a4b7b5',
-            padding: '21px 34px',
-          }}
-        >
-          See the gallery
-        </Button>
+        <ThemeProvider theme={theme}>
+          <Button
+            href="/gallery"
+            variant="contained"
+            color="primary"
+            sx={{
+              borderRadius: '300px',
+              padding: '21px 34px',
+            }}
+          >
+            See the gallery
+          </Button>
+        </ThemeProvider>
       </div>
       <div className="newsletter-button">
-        <Button
-          href="/newsletter"
-          variant="contained"
-          sx={{
-            borderRadius: '300px',
-            backgroundColor: '#5c6060',
-            padding: '13px 26px',
-            textTransform: 'none',
-          }}
-        >
-          Sign up for my newsletter
-        </Button>
+        <ThemeProvider theme={theme}>
+          <Button
+            href="/newsletter"
+            variant="contained"
+            color="secondary"
+            sx={{
+              borderRadius: '300px',
+              padding: '13px 26px',
+              textTransform: 'none',
+            }}
+          >
+            Sign up for my newsletter
+          </Button>
+        </ThemeProvider>
       </div>
     </div>
   );
