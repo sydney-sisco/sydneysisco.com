2f6e963b01c9d3f6be95d7268ac771c4ad865870
Sydney Sisco <hello@sydsis.co>
2023-01-07 10:39:50 -0800

    add glitch effect to subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,28 @@
-import './Header.css';
+import styles from './Header.module.css';
 
 
 function Header() {
 
   return (
-    <header className='header'>
-      <div className='title'>
+    <header className={styles.header}>
+      <div className={styles.title}>
         <a href="/" >
-          <h1 className='logo'>
+          <h1 className={styles.logo}>
               Sydney Sisco
           </h1>
-          <h1 className='logo'>
+          <h1 className={styles.logo}>
               Sydney Sisco
           </h1>
-          <h1 className='logo'>
+          <h1 className={styles.logo}>
               Sydney Sisco
           </h1>
         </a>
       </div>
-      <h2 className='subheading'>art by human</h2>
+      <div className={styles.subtitle}>
+        <h2 className={styles.subheading}>art by human</h2>
+        <h2 className={styles.subheading}>art by human</h2>
+        <h2 className={styles.subheading}>art by human</h2>
+      </div>
     </header>
   );
 }
